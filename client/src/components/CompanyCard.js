1cb54f8a7cbda7df8da7e15bb7a5da5d75044843
Paul N Barnes <paulieb99@gmail.com>
2021-09-22 16:01:12 -0700

    added link functionality for each company card so when clicked it will direct the url to that specific company page

--- client/src/components/CompanyCard.js
+++ client/src/components/CompanyCard.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import '../pages/company/company.css'
+import '../pages/company/company.css';
+import { Link } from 'react-router-dom';
 
 function CompanyCard(props) {
   return (
     <div className="companyCardContainer">
-      <div className="companyCardHeader">{props.name}</div>
-      <div className="companyCardDescription">{props.description}</div>
-      <div className="companyCardAvailableJobs">Jobs Available: {props.jobsLength}</div>
+      <Link to={`/company/${props.name}`}>
+        <div className="companyCardHeader">{props.name}</div>
+        <div className="companyCardDescription">{props.description}</div>
+        <div className="companyCardAvailableJobs">
+          Jobs Available: {props.jobsLength}
+        </div>
+      </Link>
     </div>
   );
 }
